e1ca72ec8a2e396ca871e14a95ada510b291c3c3
Jacek <jacek.stob@gmail.com>
2017-06-11 19:15:53 +0200

    haddock for source files

--- src/Crawler.hs
+++ src/Crawler.hs
@@ -7,18 +7,31 @@ import Text.XML.HXT.Curl
 import qualified Data.Set as Set
 import Data.List
 import UrlOps
+import Data.Tree.NTree.TypeDefs
+import Text.XML.HXT.DOM.TypeDefs
+import Text.XML.HXT.Arrow.XmlState.TypeDefs
+import Control.Arrow.IOStateListArrow
 
-walkPage :: String -> Int -> Set.Set String -> [(String, Int)] -> IO ()
+-- | Parses the page given by src and adds found links to todos with appropriate remaining depth.
+-- Then calls for the next page from todos to be visited.
+walkPage :: String           -- ^ Link to the page to be parsed.
+         -> Int              -- ^ The remaining depth of the search, for the currently parsed page.
+         -> Set.Set String   -- ^ A set of already visited links.
+         -> [(String, Int)]  -- ^ So far accumulated list of links to visit, with respective remaining depths.
+         -> IO ()            -- ^ No return value.
 walkPage src depth visited todo = do
-    let doc = readDocument [withParseHTML yes, withWarnings no, withCurl []] src
-    links <- runX $ doc >>> multi (hasName "a") >>> getAttrValue "href"
+    let doc = parsePage src
+    links <- extractLinks doc
     let newVisited = Set.insert src visited
-    putStrLn src
     let baseUrl = getBaseUrl src
     let fullLinks = map (makeFullLink baseUrl) links
-    let updatedTodo = (++) todo $ map (\x -> (x, depth-1)) (selectLinksToVisit fullLinks newVisited)
+    let updatedTodo = mergeTodos todo fullLinks depth newVisited
+    putStrLn src
     nextPage newVisited updatedTodo
 
+-- |Takes the next link from todos and if it wasn't yet visited, parses it.
+-- If visited it was, then the next link from todos is taken.
+-- Should the todos list be empty, the function returns.
 nextPage :: Set.Set String -> [(String, Int)] -> IO ()
 nextPage _ [] = return ()
 nextPage visited ((src, depth):todo) =
@@ -26,5 +39,24 @@ nextPage visited ((src, depth):todo) =
     then walkPage src depth visited todo
     else nextPage visited todo
 
+-- |Parses the given source page to XML form using HXT.
+parsePage :: String -> IOStateArrow s b XmlTree
+parsePage src = readDocument [withParseHTML yes, withWarnings no, withCurl []] src
+
+-- |Parses an XML form of a webpage's DOM structure.
+-- Returns a list of href elements of \<a\> tags.
+extractLinks :: IOSLA (XIOState ()) XmlTree (NTree XNode) -> IO [String]
+extractLinks doc = runX $ doc >>> multi (hasName "a") >>> getAttrValue "href"
+
+-- |'mergeTodos' adds newly found links as todos, with approprite remaining depth parameters.
+mergeTodos :: [(String, Int)] -- ^ Previously gathered links to skan, with remaining depth.
+           -> [String]        -- ^ Newly found links.
+           -> Int             -- ^ Remaining depth on the current level of scanning.
+           -> Set.Set String  -- ^ Set of already visited links.
+           -> [(String, Int)] -- ^ Old and new links to skan together, with respective remaining depths.
+mergeTodos todos links depth visited = (++) todos $ map (\x -> (x, depth-1)) (selectLinksToVisit links visited)
+
+-- |Extracts links not present in the given set and returns them as a list.
 selectLinksToVisit :: [String] -> Set.Set String -> [String]
 selectLinksToVisit links visited = filter (\a -> Set.notMember a visited) links
+
--- src/UrlOps.hs
+++ src/UrlOps.hs
@@ -7,12 +7,18 @@ import qualified Data.Set as Set
 import Data.List.Split (splitOn)
 import Data.List
 
+-- |Extracts the base link to a website from given url.
+--For example for https://hackage.haskell.org/package/xhtml it will return https://hackage.haskell.org
 getBaseUrl :: String -> String
 getBaseUrl link | isPrefixOf "http" link = case split of (x:y:z:_) -> x ++ "//" ++ z
                 | otherwise = head $ split
                 where split = splitOn "/" link
 
-makeFullLink :: String -> String -> String
+-- | Given a path extracted from an \<a\> tag's href element and a link to its base source page,
+-- returns an absolute link to a page referenced by that element.
+makeFullLink :: String  -- ^ Link to the base source page (ex. stackoverflow.com).
+             -> String  -- ^ Path extracted from an \<a\> tag.
+             -> String  -- ^ Absolute link to the referenced page.
 makeFullLink baseUrl ('/':'/':xs) = "http://" ++ xs
 makeFullLink baseUrl ('/':link) = baseUrl ++ ('/':link)
 makeFullLink baseUrl _ = baseUrl
